13a5a3dfd3da7676e83c92e5705d7c1ac93a8eab
fati-p <143958473+fati-p@users.noreply.github.com>
2023-11-03 19:48:01 -0600

    distancia

--- distancia_puntos.cs
+++ distancia_puntos.cs
@@ -1,1 +1,38 @@
+using System;
+
+public class Punto
+{
+    public decimal x;
+    public decimal y;
+    public decimal z;
+
+    public Punto(decimal x, decimal y, decimal z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double MedirDistancia(Punto otroPunto)
+    {
+        decimal dx = otroPunto.x - this.x;
+        decimal dy = otroPunto.y - this.y;
+        decimal dz = otroPunto.z - this.z;
+
+        return Math.Sqrt((double)(dx * dx + dy * dy + dz * dz));
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Punto puntoOrigen = new Punto(0, 0, 0);
+        Punto puntoDestino = new Punto(3, 4, 5);
+
+        double distancia = puntoOrigen.MedirDistancia(puntoDestino);
+
+        Console.WriteLine("La distancia entre el punto de origen y el punto de destino es " + distancia);
+    }
+}
 
